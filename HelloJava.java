6a68f807ca3941cc522acf2c29ceaedbeb903bad
edwin15r <edwin15p@gmail.com>
2024-12-16 15:21:28 -0500

    primer commit

--- HelloJava.java
+++ HelloJava.java
@@ -2,7 +2,7 @@ public class HelloJava {
 	
 	public static void main(String[] args) {
 
-		System.out.println("Hello Java Se hizo el primer cambio");
+		System.out.println("Hello Java Se hizo el primer cambio,2do cambio");
 		  
 		  
 	
